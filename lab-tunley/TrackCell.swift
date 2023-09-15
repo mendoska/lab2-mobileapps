ef868e44fda7eeded94bbad4e8af76a659f97f31
Alexander Mendoza <alexandermen2017@fau.edu>
2023-09-15 18:58:58 -0400

    Initial Commit

---
+++ lab-tunley/TrackCell.swift
@@ -0,0 +1,34 @@
+//
+//  TrackCell.swift
+//  lab-tunley
+//
+//  Created by Alexander Mendoza on 9/15/23.
+//
+
+import UIKit
+
+class TrackCell: UIViewController {
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
